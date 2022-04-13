f27e682614c69bab70ca135fe7c132962ebf422e
Tim Johnson <tim@timbob.com>
2022-04-12 21:05:59 -0500

    Add project files.

---
+++ Models/MenuItem.cs
@@ -0,0 +1,11 @@
+﻿namespace DCYC.Signage.Models
+{
+    public class MenuItem
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public DateTime Expiration { get; set; }
+
+    }
+}
